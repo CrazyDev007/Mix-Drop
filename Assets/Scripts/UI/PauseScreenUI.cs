d2f5a3fc34b04dec1c52e8fcdb44250575609c16
CrazyDev007 <gajendramr2@gmail.com>
2025-04-01 12:23:47 +0530

    Game Restart and Home

--- Assets/Scripts/UI/PauseScreenUI.cs
+++ Assets/Scripts/UI/PauseScreenUI.cs
@@ -14,29 +14,12 @@ namespace UI
 
         public void OnClickBtnRestart()
         {
-            // Restart the current level/game
-            // Reset timescale in case it was paused
-            Time.timeScale = 1f;
-
-            // You might want to use a scene manager or game manager to handle the restart
-            // For example: GameManager.Instance.RestartGame();
-
-            // For now, we can reload the current scene
-            SceneManager.LoadScene(
-                SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
 
         public void OnClickBtnHome()
         {
-            // Return to the home/main menu
-            // Reset timescale in case it was paused
-            Time.timeScale = 1f;
-
-            // You might want to load the main menu scene
-            // For example: SceneManager.LoadScene("MainMenu");
-
-            // Or use a game manager to handle the transition
-            // For example: GameManager.Instance.ReturnToMainMenu();
+            SceneManager.LoadSceneAsync("Main");
         }
 
         public override void SetActive(bool active)
